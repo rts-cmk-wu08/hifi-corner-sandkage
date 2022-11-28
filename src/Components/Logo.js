dd3934b2486c1656abe4bd77063a63eb5aa01903
nephiejoy <stephanie@falkberg.dk>
2022-11-28 12:58:29 +0100

    Tilføjet Header

---
+++ src/Components/Logo.js
@@ -0,0 +1,10 @@
+import "./Logo.css"
+
+const Logo = () => {
+    return ( 
+        <p className="logo">Hej</p>
+        // Kunne ikke importere logoet så har brugt et p tag for at vise at komponentet virker
+     );
+}
+ 
+export default Logo;
---
+++ src/Templates/Header.js
@@ -0,0 +1,12 @@
+import "./Header.css"
+import Logo from "../Components/Logo";
+
+const Header = () => {
+    return ( 
+        <header className="header">
+           <Logo /> 
+        </header>
+     );
+}
+ 
+export default Header;
