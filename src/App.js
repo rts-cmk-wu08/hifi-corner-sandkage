d51e99e09cca012c7bc28151631f3dca6dbcb3e2
Daniel20122001 <danielhoven12@gmail.com>
2022-11-28 13:18:01 +0100

    Tilføjet videoen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-   
+
+    <video width="1920" height="1080" controls >
+      <source src="../src/Components/video" type="video/mp4"/>
+    </video>
+
     </div>
   );
 }
