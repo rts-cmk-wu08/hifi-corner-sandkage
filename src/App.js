76e480b256f2c0fb95a52f775a924750ce60e57f
Daniel20122001 <danielhoven12@gmail.com>
2022-11-28 14:16:20 +0100

    Tilføjet videoen

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ function App() {
   return (
     <div className="App">
 
-    <video width="1920" height="1080" controls >
-      <source src="/src/Components/video/HiFi-Corner-Web-Video.mp4" type="video/mp4"/>
-    </video>
+<video width="1920" height="1080" controls>
+  <source src="/src/Components/video/HiFi-Corner-Web-Video.mp4" type="video/mp4">
+  </source>
+</video> 
 
     </div>
   );
