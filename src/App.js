2ac9284180c881f85a9026621b3158f477cab2b0
Kjartan <kjartismarti@gmail.com>
2022-11-29 13:38:11 +0100

    footer styling færdig.. mangler links

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Footer from './Templates/Footer';
+import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-   
+
+      <Outlet/>
+      <Footer/>
     </div>
   );
 }
