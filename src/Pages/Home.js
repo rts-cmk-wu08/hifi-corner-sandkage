f223a194b9c0fd4b54f79ff200479f3bb07264e9
Kjartan <kjartismarti@gmail.com>
2022-11-29 14:12:28 +0100

    added home

---
+++ src/Pages/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return ( 
+        <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, voluptatem atque! Earum, placeat quae voluptatibus nihil quaerat voluptatum reiciendis minima!</p></div>
+     );
+}
+ 
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Home from './Pages/Home';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-
+      <Route index element={<Home/>}/>
     </Route>
   )
 )
