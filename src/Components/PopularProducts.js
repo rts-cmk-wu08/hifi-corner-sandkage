79d449b9401a3d3733102b72cf399c94fa316b5a
Daniel20122001 <danielhoven12@gmail.com>
2022-12-06 13:39:50 +0100

    changes

--- src/Components/PopularProducts.js
+++ src/Components/PopularProducts.js
@@ -1,7 +1,10 @@
+import "../global.scss"
+import "./PopularProducts.scss";
+import "./ProductCards.scss"
 import axios from "axios";
 import { useEffect, useState } from "react";
 import ReadMoreButton from "./ReadMoreButton";
-
+import SeeAllProductsButton from "./SeeAllProductsButton";
 
 
 const PopularProducts = () => {
@@ -18,13 +21,17 @@ const PopularProducts = () => {
 
     return loading ? <p>Loading...</p> :(
         <section>
+            <div>
+            <h1>POPULAR PRODUCTS</h1>
+            <SeeAllProductsButton/>
+            </div>
             {
                 products.map((product) => (
-                    <article>
-                        <img src={`http://localhost:4000${product.images}`} alt={`${product.title}`}/>
+                    <article className="productcard productcard_popularproducts" key={product.id}>
+                        <img src={`${product.images}`} alt={`${product.title}`}/>
                         <h3>{product.title}</h3>
-                        <p>£ {product.price}</p>
-                        <ReadMoreButton/>
+                            <p>£ {product.price}</p>
+                            <ReadMoreButton id={product.id}/>
                     </article>
                 ))}
         </section>
