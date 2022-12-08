67b7e015006a9d6be1da8a8cc558f92bb849993e
SebCodiing <Sebberoh@gmail.com>
2022-12-08 14:18:21 +0100

    made and added scss to whatwedo container

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,14 +1,16 @@
 import PopularProducts from "../Components/PopularProducts";
 import Video from "../Components/Video";
-import "./Home.scss"
+import WhatWeDo from "../Components/WhatWeDo";
+import "./Home.scss";
 
 const Home = () => {
-    return (
-        <div className="home_container">
-        <Video/>
-        <PopularProducts/>
-        </div>
-     );
-}
- 
+  return (
+    <div className="home_container">
+      <Video />
+      <PopularProducts />
+      <WhatWeDo />
+    </div>
+  );
+};
+
 export default Home;
