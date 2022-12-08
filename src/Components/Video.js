653653148057fb1681c75346b7bc2d4859d93dca
Daniel20122001 <danielhoven12@gmail.com>
2022-12-08 11:14:58 +0100

    Edited in video

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,13 +1,23 @@
 // --- OBS! Lyden på vidoen skal være slået fra med koden "muted" før vidoen afspilles automatisk --- //
 
-import introvideo from "./video/HiFi-Corner-Web-Video.mp4"
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./Video.scss"
 
 const Video = () => {
-    return ( 
+    const [video, setVideo] = useState();
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        axios("http://localhost:4000/assets/hero_video.mp4")
+        .then(response => setVideo(response.data))
+        .finally(() => setLoading(false))
+    }, []);
+    return loading ? <p>Loading...</p> : (
 
 <section className="introvideo">
     <video width="100%" height="auto" autoPlay loop muted>
-    <source src={introvideo}></source>
+    <source src={video.video}></source>
     </video>
 </section>
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,16 @@
 import PopularProducts from "../Components/PopularProducts";
+import Video from "../Components/Video";
 
 const Home = () => {
     return ( 
+        <>
         <div><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, voluptatem atque! Earum, placeat quae voluptatibus nihil quaerat voluptatum reiciendis minima!</p>
         <PopularProducts/>
         </div>
+        <div>
+        <Video/>
+        </div>
+        </>
      );
 }
  
