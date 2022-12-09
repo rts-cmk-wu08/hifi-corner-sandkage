ad8e712f97c53624c5d1f9a6ec507bee2307b848
SebCodiing <Sebberoh@gmail.com>
2022-12-09 12:12:39 +0100

    added FaShoppingCart icon to header

--- src/Templates/Header.js
+++ src/Templates/Header.js
@@ -1,14 +1,16 @@
-import "./Header.scss"
+import "./Header.scss";
 import Logo from "../Components/Logo";
 import Navigation from "./Navigation";
+import { FaShoppingCart } from "react-icons/fa";
 
 const Header = () => {
-    return ( 
-        <header className="header">
-           <Logo /> 
-           <Navigation />
-        </header>
-     );
-}
- 
+  return (
+    <header className="header">
+      <Logo />
+      <Navigation />
+      <FaShoppingCart />
+    </header>
+  );
+};
+
 export default Header;
