626ba7ea123c01668c299b74b6c41f749e1aa593
Kjartan <kjartismarti@gmail.com>
2022-12-12 11:01:42 +0100

    added some functions and styling for product page

--- src/Templates/Navigation.js
+++ src/Templates/Navigation.js
@@ -5,7 +5,7 @@ import "./Navigation.scss"
 const Navigation = () => {
     return ( 
         <nav className="navbar">
-            <Link className="navLink" to="/"><NavItem title="Shop" /></Link>
+            <Link className="navLink" to="/shop"><NavItem title="Shop" /></Link>
             <Link className="navLink" to="/aboutus"><NavItem title="About Us" /></Link>
             <Link className="navLink" to="/contactus"><NavItem title="Contact Us" /></Link>
         </nav>
