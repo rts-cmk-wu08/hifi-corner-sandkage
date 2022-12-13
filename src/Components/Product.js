f5d73edd522f7700216fe46c6b60004b490a6015
SebCodiing <Sebberoh@gmail.com>
2022-12-13 12:40:57 +0100

    Added linear-gradient to Product.scss

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -16,7 +16,25 @@ const Product = () => {
         <div className="productDescription">
           <h3>{productData.title}</h3>
           <p>{productData.description}</p>
-          <p>£ {productData.price}</p>
+          <div className="productBtn">
+            <div className="colorBtn_black color"></div>
+            <div className="text">
+              <p>Black</p>
+            </div>
+          </div>
+          <div className="productBtn">
+            <div className="colorBtn_silver color"></div>
+            <div className="text">
+              <p>Silver</p>
+            </div>
+          </div>
+          <div className="productBtn">
+            <div className="colorBtn_gold color"></div>
+            <div className="text">
+              <p>Gold</p>
+            </div>
+          </div>
+          <p className="productPrice">£ {productData.price}</p>
           <p>{productData.stock}</p>
         </div>
       </article>
