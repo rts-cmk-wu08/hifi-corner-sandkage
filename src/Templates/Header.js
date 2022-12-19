f11992ce3aabae615c97afe5e1e2613e89623d60
Kjartan <kjartismarti@gmail.com>
2022-12-19 09:30:01 +0100

    styling for counter added

--- src/Templates/Header.js
+++ src/Templates/Header.js
@@ -2,8 +2,8 @@ import "./Header.scss";
 import Logo from "../Components/Logo";
 import Navigation from "./Navigation";
 import { FaUser } from "react-icons/fa";
-import { FaShoppingCart } from "react-icons/fa";
 import SearchBar from "../Components/SearchBar";
+import ShoppingCart from "../Components/Shoppingcart";
 
 const Header = () => {
   return (
@@ -13,7 +13,8 @@ const Header = () => {
       <div className="header_icons">
         <SearchBar placeholder="Search product..." />
         <FaUser className="user_icon" />
-        <FaShoppingCart className="shoppingcart_icon" />
+        <ShoppingCart />
+
       </div>
     </header>
   );
