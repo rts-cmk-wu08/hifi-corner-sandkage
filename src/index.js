e956ff27b9e8e7c840c14d98f1dcc0233674ab0b
[Kjartan Roberts] <[kjartanuroberts@gmail.com]>
2022-12-21 11:57:18 +0100

    fixed productpage

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import Home from './Pages/Home';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
-import Product, {loader as getProductDetails} from './Components/Product';
+import Product, { loader as getProductDetails } from './Components/Product';
 import Faq from "./Pages/Faq.js"
 import Productpage from './Pages/Productpage';
 import About from './Pages/About';
@@ -12,8 +12,8 @@ import About from './Pages/About';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route index element={<Home/>}/>
-      <Route path="/product/:id" element={<Product/>} loader={getProductDetails}/>
+      <Route index element={<Home />} />
+      <Route path="/product/:id" element={<Product />} loader={getProductDetails} />
       <Route path='/faq' element={<Faq />} />
       <Route path='/shop' element={<Productpage />} />
       <Route path='/aboutus' element={<About />} />
